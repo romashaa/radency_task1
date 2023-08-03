9b4fdff7701c4d9afa67a5e9f80beb3019b42eb0
romashaa <mariia.romanenko@ukma.edu.ua>
2023-08-03 15:15:20 +0300

    created table with categories and functionality to archive tacks

--- script/script.js
+++ script/script.js
@@ -1,33 +1,94 @@
+const categories =[
+    "Random Thought",
+    "Task",
+    "Idea"
+]
+
+const selectElement = document.getElementById('taskCategory');
+
+categories.forEach((option) => {
+    const optionElement = document.createElement('option');
+    optionElement.text = option;
+    optionElement.value = option;
+    selectElement.appendChild(optionElement);
+});
+
+
 const notes = [
-    { name: "Note 1", date: "2023-07-29", category: "Random Thought", content: "Content of Note 1" },
-    { name: "Note 2", date: "2023-07-30", category: "Task", content: "Content of Note 2" },
-    { name: "Note 3", date: "2023-07-31", category: "Random Thought", content: "Content of Note 3" },
-    { name: "Note 4", date: "2023-08-01", category: "Idea", content: "Content of Note 4" },
-    { name: "Note 5", date: "2023-08-02", category: "Task", content: "Content of Note 5" }
+    { name: "Note 1", date: "2023-07-29", category: categories[0], content: "Content of Note 1", dates:"32532", archived: false },
+    { name: "Note 2", date: "2023-07-30", category: categories[2], content: "Content of Note 2", dates:"32532", archived: false },
+    { name: "Note 3", date: "2023-07-31", category: categories[1], content: "Content of Note 3", dates:"32532", archived: false },
+    { name: "Note 4", date: "2023-08-01", category: categories[0], content: "Content of Note 4", dates:"32532", archived: false },
+    { name: "Note 5", date: "2023-08-02", category: categories[1], content: "Content of Note 5", dates:"32532", archived: false }
 ];
+// const archivedNotes = [];
+const archiveTable = document.querySelector("#archiveDiv");
+function viewArchive(){
+    archiveTable.style.display="block";
+}
+function closeArchive(){
+    archiveTable.style.display="none";
+}
+
+function archiveRow(index) {
+    notes[index].archived = true;
+    populateArchivedTable();
+    populateTable();
+    updateCategoriesTable();
+}
+
+function unarchiveRow(index) {
+    notes[index].archived = false;
+    populateArchivedTable();
+    populateTable();
+    updateCategoriesTable();
+}
+function populateArchivedTable() {
+    const tbody = document.querySelector("#archivedNotesTable tbody");
+    tbody.innerHTML = "";
+
+    notes.forEach((note, index) => {
+        if (note.archived) {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+            <td>${note.name}</td>
+            <td>${note.date}</td>
+            <td>${note.category}</td>
+            <td>${note.content}</td>
+            <td></td>
+            <td>
+                <i onclick="unarchiveRow(${index})" class="fa fa-undo" aria-hidden="true"></i>
+            </td>
+        `;
+            tbody.appendChild(row);
+        }
+    });
+}
+/////////////////////////////////////////////////////////
 function populateTable() {
     const tbody = document.querySelector("#notesTable tbody");
     tbody.innerHTML = "";
 
     notes.forEach((note,index) => {
-        const row = document.createElement("tr");
-        row.innerHTML = `
-      <td>${note.name}</td>
-      <td>${note.date}</td>
-      <td>${note.category}</td>
-      <td>${note.content}</td>
-      <td>
-          <i data-toggle="modal" data-target="#modal" onclick="editRow(${index})" class="fa fa-pencil" aria-hidden="true"></i>
-          <i class="fa fa-archive" aria-hidden="true"></i>
-          <i onclick="deleteRow(${index})" class="fa fa-trash-o" aria-hidden="true"></i>
-      </td>
-    `;
-        tbody.appendChild(row);
+        if (!note.archived) {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+              <td>${note.name}</td>
+              <td>${note.date}</td>
+              <td>${note.category}</td>
+              <td>${note.content}</td>
+              <td>${note.dates}</td>
+              <td>
+                  <i data-toggle="modal" data-target="#modal" onclick="editRow(${index})" class="fa fa-pencil" aria-hidden="true"></i>
+                  <i onclick="archiveRow(${index})" class="fa fa-archive" aria-hidden="true"></i>
+                  <i onclick="deleteRow(${index})" class="fa fa-trash-o" aria-hidden="true"></i>
+              </td>
+        `;
+            tbody.appendChild(row);
+        }
     });
 }
-document.addEventListener("DOMContentLoaded", () => {
-    populateTable();
-});
+/////////////////////////////////////////////////////
 const editButtons = document.querySelectorAll(".fa-pencil");
 editButtons.forEach((editButton, index) => {
     editButton.onclick = () => editRow(index);
@@ -43,6 +104,7 @@ const errorMessage = document.querySelector("#error-message");
     function deleteRow(index) {
         notes.splice(index, 1);
         populateTable();
+        updateCategoriesTable();
     }
 
     /////////////////////////////////////////////////////////
@@ -77,6 +139,7 @@ function editRow(index) {
     closeButton.addEventListener("click", () => {
         modal.style.display = "none";
         errorMessage.style.display = "none";
+        document.body.style.overflow = "auto";
     });
 
     saveNoteButton.addEventListener("click", () => {
@@ -96,11 +159,61 @@ function editRow(index) {
             }
             populateTable();
             clearFormFields();
+            updateCategoriesTable();
             modal.style.display = 'none';
             modal.classList.remove('show');
             let modalBackdrop = document.querySelector('.modal-backdrop');
             modalBackdrop.style.display = 'none';
+            document.body.style.overflow = "auto";
         } else{
             errorMessage.style.display = "block";
         }
     });
+
+function updateCategoriesTable() {
+    const categoriesTable = document.querySelector("#categoriesTable");
+    const categories = {};
+
+    notes.forEach((note) => {
+        if (!note.archived) {
+            if (!categories[note.category]) {
+                categories[note.category] = {
+                    active: 1,
+                    archived: 0,
+                };
+            } else {
+                categories[note.category].active++;
+            }
+        } else {
+            if (!categories[note.category]) {
+                categories[note.category] = {
+                    active: 0,
+                    archived: 1,
+                };
+            } else {
+                categories[note.category].archived++;
+            }
+        }
+    });
+
+    // Clear the existing rows in the table body
+    const tbody = categoriesTable.querySelector("tbody");
+    tbody.innerHTML = "";
+
+    // Populate the table with the updated category data
+    Object.keys(categories).forEach((category) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td>${category}</td>
+            <td>${categories[category].active}</td>
+            <td>${categories[category].archived}</td>
+        `;
+        tbody.appendChild(row);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    populateTable();
+    populateArchivedTable();
+    updateCategoriesTable();
+});
